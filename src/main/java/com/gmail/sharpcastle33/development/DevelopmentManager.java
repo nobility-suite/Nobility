4719c0e24d42cdd7375b18ecdb709bad65f148c1
ukulelelesheep <wmconorbrown@gmail.com>
2019-08-23 11:10:11 -0500

    Added Food Productivity

--- src/main/java/com/gmail/sharpcastle33/development/DevelopmentManager.java
+++ src/main/java/com/gmail/sharpcastle33/development/DevelopmentManager.java
@@ -8,6 +8,7 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
 
 import com.gmail.sharpcastle33.Nobility;
 import com.gmail.sharpcastle33.estate.Estate;
@@ -30,29 +31,52 @@ public class DevelopmentManager {
     public int calculateGains(Estate estate, String resource) {
     	double regionResource = estate.getRegion().getResource(resource);
     	double level = 1; //Add to the development class
-    	double maximum = 5; //5 is arbitrary. Tweak later on.
+    	double maximum = 10; //5 is arbitrary. Tweak later on.
     	double totalCompetingEstates = 0;
+    	double rounded;
+    	double productivity = calculateFoodProductivity(estate);
     	int returnAmount;
     	
     	for (Estate otherEstate : Nobility.estateMan.estates) {
     		if (estate.getRegion().equals(otherEstate.getRegion())) {    			
-    			totalCompetingEstates++; //multiply by each estate's level modifier later on
+    			totalCompetingEstates =+ totalCompetingEstates*calculateFoodProductivity(otherEstate); 
+    			//multiply by each estate's level modifier later on    			
     			//level can be 0 if the development is inactive
     		}
     	}
     	
     	if (totalCompetingEstates * maximum <= regionResource) {
-    		returnAmount = (int) (maximum * level);
+    		rounded = Math.floor(maximum * level * productivity);
+    		returnAmount = (int) rounded;
     		return returnAmount;
-
     	}
     	
     	if (totalCompetingEstates * maximum > regionResource) {
-    		returnAmount = (int) ((regionResource * level) / totalCompetingEstates);
+    		rounded = Math.floor((regionResource * level * productivity) / totalCompetingEstates);
+    		returnAmount = (int) rounded;
     		return returnAmount;
     	} else {    		
     		return 0;
     	}    	
+    }
+    
+    public double calculateFoodProductivity(Estate estate) {
+    	//if number of types of foods equals 
+    	Inventory inv = getStorehouseInventory(estate);
+    	double productivity = .4d;
+    	if (inv.containsAtLeast(new ItemStack(Material.WHEAT), 10)) {
+    		productivity += .2d;
+    	}
+    	if (inv.containsAtLeast(new ItemStack(Material.BEEF), 10)) {
+    		productivity += .2d;
+    	}
+    	if (inv.containsAtLeast(new ItemStack(Material.SWEET_BERRIES), 10)) {
+    		productivity += .2d;
+    	}
+    	
+    	return productivity;
+
+    	
     }
     
     //Change if storehouse inventory and creation is changed
