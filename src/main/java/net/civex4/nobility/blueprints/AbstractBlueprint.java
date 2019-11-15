976fc0767bb86b6d8658ca23cdec9e0621aca72d
ukulelelesheep <wmconorbrown@gmail.com>
2019-11-15 17:18:34 -0600

    Reformatting
    Added human readable variable names
    Used CivModCore functions
    Cleaned up other things

--- src/main/java/net/civex4/nobility/blueprints/AbstractBlueprint.java
+++ src/main/java/net/civex4/nobility/blueprints/AbstractBlueprint.java
@@ -2,12 +2,19 @@ package net.civex4.nobility.blueprints;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Logger;
 
+import javax.sound.midi.MidiDevice.Info;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Warning;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import net.md_5.bungee.api.ChatColor;
+import vg.civcraft.mc.civmodcore.api.ItemAPI;
 	
 public class AbstractBlueprint {
 	private static final String AMOUNT_DELIMITER = "x ";
@@ -37,17 +44,16 @@ public class AbstractBlueprint {
 	 *   36x Liferod
 	 */
 	
-	public static ItemStack getItemStackFromName(String s) {
+	public static ItemStack getItemStackFromName(String str) {
 		//TODO, this should be a lookup from the yaml doc.
-		ItemStack i = new ItemStack(Material.PAPER);
-		ItemMeta m = i.getItemMeta();
-		m.setDisplayName(s);
-		i.setItemMeta(m);
-		return i;
+		ItemStack item = new ItemStack(Material.PAPER);
+		ItemAPI.setDisplayName(item, str);
+		return item;
 	}
+	// TODO Custom Items need
 	
-	public AbstractBlueprint(ItemStack result, HashMap<ItemStack, Integer> ingredients, int runs, 
-			String name, int resultAmount) {
+	public AbstractBlueprint(ItemStack result, HashMap<ItemStack, Integer> ingredients, 
+			int runs, String name, int resultAmount) {
 		
 		this.result = result;
 		this.ingredients = ingredients;
@@ -65,97 +71,87 @@ public class AbstractBlueprint {
 	public ItemStack parseBlueprintToItem() {
 		
 		//Parse name, runs, and result.
-		String parsed_name = this.name;
-		String parsed_runs = RUNS_PREFIX + Integer.toString(this.runs);
-		String parsed_result = RESULT_PREFIX + RESULT_AMOUNT_PREFIX + Integer.toString(this.resultAmount)+ AMOUNT_DELIMITER + this.result.getItemMeta().getDisplayName();
+		String parsedName = this.name;
+		String parsedRuns = RUNS_PREFIX + Integer.toString(this.runs);
+		String parsedResult = RESULT_PREFIX 
+				+ RESULT_AMOUNT_PREFIX 
+				+ Integer.toString(this.resultAmount)
+				+ AMOUNT_DELIMITER 
+				+ ItemAPI.getDisplayName(this.result);
 	 
 		//TODO not needed for first test
-		String parsed_time = TIME_INSTANT;
-		
+		String parsedTime = TIME_INSTANT;
 		
 		//Parse all ingredients
-		ArrayList<String> parsed_ingredients = new ArrayList<String>();
-		String ingredient_delimiter = INGREDIENTS_LINE_DELIMITER;
-		
-		for(ItemStack i : ingredients.keySet()) {
-			String parsed_i = INGREDIENT_AMOUNT_PREFIX + Integer.toString(ingredients.get(i)) + AMOUNT_DELIMITER + i.getItemMeta().getDisplayName();
-			parsed_ingredients.add(parsed_i);
+		List<String> parsedIngredients = new ArrayList<>();
+
+		for(ItemStack item : ingredients.keySet()) {
+			String parsedItem = INGREDIENT_AMOUNT_PREFIX 
+					+ Integer.toString(ingredients.get(item)) 
+					+ AMOUNT_DELIMITER 
+					+ ItemAPI.getDisplayName(item);
+			parsedIngredients.add(parsedItem);
 		}
+
+		ItemStack returnItem = new ItemStack(Material.PAPER);
+		ItemAPI.setDisplayName(returnItem, parsedName);
+		ItemAPI.addLore(returnItem, parsedResult, parsedRuns, parsedTime, INGREDIENTS_LINE_DELIMITER);
+		ItemAPI.addLore(returnItem, parsedIngredients);
 		
-		ItemStack ret = new ItemStack(Material.PAPER, 1);
-		ItemMeta meta = ret.getItemMeta();
-		meta.setDisplayName(parsed_name);
-		
-		ArrayList<String> bplore = new ArrayList<String>();
-		
-		//Add all parsed information to item meta.
-		
-		bplore.add(parsed_result);
-		bplore.add(parsed_runs);
-		bplore.add(parsed_time);
-		bplore.add(ingredient_delimiter);
-		
-		for(String s : parsed_ingredients) {
-			bplore.add(s);
-		}
-		
-		meta.setLore(bplore);
-		ret.setItemMeta(meta);
-				
-		return ret;
-		
+		return returnItem;
 	}
 	
 	/**
 	 * Parse an ItemStack into an AbstractBlueprint.
-	 * @param i, Blueprint Itemstack
+	 * @param item, Blueprint Itemstack
 	 * @return AbstractBlueprint with given information.
 	 * @author Sharpcastle33
 	 */
 	
 	public static AbstractBlueprint parseBlueprintFromItem(ItemStack item) {
-		java.util.List<String> lore = item.getItemMeta().getLore();
-		String itemName = item.getItemMeta().getDisplayName();
+		String itemName = ItemAPI.getDisplayName(item);
 		
+		List<String> lore = ItemAPI.getLore(item);
+		if (lore.size() < 4) {
+			Bukkit.getLogger().warning("Only Blueprint Items can be parsed");
+			return null;
+		}
+
 		//Parse list into equivalent strings
-		String parsed_result = lore.get(0);
-		String parsed_runs = lore.get(1);
-		String parsed_time = lore.get(2);
+		String parsedResult = lore.get(0);
+		String parsedRuns = lore.get(1);
+		String parsedTime = lore.get(2);
 		//Ingredient Delimiter = Line 3
 		
-		ArrayList<String> ingredient_strings = new ArrayList<String>();
-		
-		for(int i = 4; i < lore.size(); i++) {
-			ingredient_strings.add(lore.get(i));
-		}
+		List<String> ingredientStrings = new ArrayList<>();
+		ingredientStrings.addAll(4, lore);
 		
 		//Parse strings into AbstractBlueprint information
 		
 		//Result Item
-		parsed_result.replaceFirst(RESULT_PREFIX, "");
-		String[] result_arr = parsed_result.split(RESULT_AMOUNT_PREFIX);
-		int result_amount = Integer.parseInt(result_arr[0]);
-		parsed_result = result_arr[1];
-		ItemStack result = getItemStackFromName(parsed_result);
+		parsedResult.replaceFirst(RESULT_PREFIX, "");
+		String[] numberAndResult = parsedResult.split(RESULT_AMOUNT_PREFIX);
+		int resultAmount = Integer.parseInt(numberAndResult[0]);
+		parsedResult = numberAndResult[1];
+		ItemStack result = getItemStackFromName(parsedResult);
 		
 		//Runs Amount
-		parsed_runs.replaceFirst(RUNS_PREFIX, "");
-		int runs_amount = Integer.parseInt(parsed_runs);
+		parsedRuns.replaceFirst(RUNS_PREFIX, "");
+		int runsAmount = Integer.parseInt(parsedRuns);
 		
 		//ParsedTime is currently unused
 		
-		//Ingredients Map
-		HashMap<ItemStack, Integer> ing = new HashMap<ItemStack, Integer>();
+		HashMap<ItemStack, Integer> ingredientsMap = new HashMap<ItemStack, Integer>();
 		
-		for(String s : ingredient_strings) {
-			s.replaceFirst(INGREDIENT_AMOUNT_PREFIX, "");
-			String[] s_arr = s.split(AMOUNT_DELIMITER);
-			int i_amt = Integer.parseInt(s_arr[0]);
-			String i_name = s_arr[1];
-			ing.put(getItemStackFromName(i_name), i_amt);
+		for(String str : ingredientStrings) {
+			str.replaceFirst(INGREDIENT_AMOUNT_PREFIX, "");
+			String[] amountAndName = str.split(AMOUNT_DELIMITER);
+			int amount = Integer.parseInt(amountAndName[0]);
+			String name = amountAndName[1];
+			ingredientsMap.put(getItemStackFromName(name), amount);
 		}
 		
-		return new AbstractBlueprint(result, ing, runs_amount, itemName, result_amount);
+		return new AbstractBlueprint(result, ingredientsMap, runsAmount, itemName, resultAmount);
 	}
 	
 }
