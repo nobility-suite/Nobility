260c5a9034d60992d696fad39fac70f559279536
Stephen / Sharp <Sharpcastle33@users.noreply.github.com>
2020-07-28 16:27:22 -0400

    Cannon management

--- src/main/java/net/civex4/nobility/cannons/CannonManager.java
+++ src/main/java/net/civex4/nobility/cannons/CannonManager.java
@@ -1,5 +1,40 @@
 package net.civex4.nobility.cannons;
 
-public class CannonManager {
+import java.util.ArrayList;
 
+import org.bukkit.Location;
+
+public class CannonManager {
+	
+	public ArrayList<Cannon> activeCannons;
+
+	public CannonManager() {
+		this.activeCannons = new ArrayList<Cannon>();
+	}
+	
+	public void addCannon(Cannon c) {
+		this.activeCannons.add(c);
+	}
+	
+	public void removeCannon(Cannon c) {
+		this.activeCannons.remove(c);
+	}
+	
+	public void dismantleCannon(Location loc) {
+		//TODO
+	}
+	
+	public boolean isCannon(Location loc) {
+		return false;
+		//TODO
+	}
+	
+	public Cannon getCannon(Location loc) {
+		return null;
+		//TODO
+	}
+	
+	public void damageCannon(Cannon c, int amt) {
+		//TODO
+	}
 }
